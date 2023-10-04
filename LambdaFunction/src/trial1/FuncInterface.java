8725a830419677867c2754b9fdab1b104f8b3d34
Pralekh <pralekhvrambhad@gmail.com>
2023-10-04 11:03:12 +0530

    message

---
+++ LambdaFunction/src/trial1/FuncInterface.java
@@ -0,0 +1,14 @@
+package trial1;
+
+public interface FuncInterface {
+	void function1(int x);
+
+	int operation(int a, int b);
+//	
+//	default void normalFun() {
+//		System.out.println("Hello");
+//	}
+	
+	
+	
+}
